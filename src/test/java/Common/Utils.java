dd4562c73110c5a40d2699594721535d8da249c6
AMajkut <32749413+AMajkut@users.noreply.github.com>
2021-10-19 15:46:18 +0200

    dodano klase wspolna do uruchamiania driveru

--- src/test/java/Common/Utils.java
+++ src/test/java/Common/Utils.java
@@ -1,4 +1,27 @@
 package Common;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
 public class Utils {
+
+    public static WebDriver setUpDriver() {
+        System.setProperty("webdriver.gecko.driver",
+                "src/main/resources/drivers/geckodriver.exe");
+
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        return driver;
+    }
+
+    public static String generateEmail() {
+        Random random = new Random();
+        int randomNumber = random.nextInt(10000);
+        return "test" + randomNumber + "mail.com";
+    }
+
 }
