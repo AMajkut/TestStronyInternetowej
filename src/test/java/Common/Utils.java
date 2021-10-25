ce27d13c69795d306b14c5c686988008b8999b62
AMajkut <32749413+AMajkut@users.noreply.github.com>
2021-10-25 18:58:51 +0200

    dodano klase Screenshot oraz uzupełniono klase Utils

--- src/test/java/Common/Utils.java
+++ src/test/java/Common/Utils.java
@@ -3,6 +3,7 @@ package Common;
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.concurrent.TimeUnit;
@@ -22,9 +23,19 @@ public class Utils {
         return driver;
     }
 
+    public static WebDriver setUpChromeDriver() {
+        System.setProperty("webdriver.chrome.driver",
+                "src/main/resources/drivers/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        return driver;
+    }
+
     @After
     public void close() {
-        driver.quit();
+//        driver.quit();
     }
 
 }
